b572415b0236d652553e998f3646803bcd75261f
AjinkyaDahiwal <144662188+AjinkyaDahiwal@users.noreply.github.com>
2025-07-24 17:43:58 +0530

    Update json_to_parquet_converter.py

--- Youtube-data-pipeline/lambda/json_to_parquet_converter.py
+++ Youtube-data-pipeline/lambda/json_to_parquet_converter.py
@@ -10,17 +10,17 @@ os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name']
 os_input_write_data_operation = os.environ['write_data_operation']
 
 def lambda_handler(event, context):
-    # Added by Ajinkya - Extract bucket and file info from S3 event
+    # Extract bucket and file info from S3 event
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
-        # Added by Ajinkya - Read JSON file from S3
+        #  Read JSON file from S3
         df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
         
-        # Added by Ajinkya - Extract nested 'items' array from JSON structure
+        #  Extract nested 'items' array from JSON structure
         df_step_1 = pd.json_normalize(df_raw['items'])
         
-        # Added by Ajinkya - Convert to Parquet and save to cleansed bucket
+        #  Convert to Parquet and save to cleansed bucket
         wr_response = wr.s3.to_parquet(
             df=df_step_1,
             path=os_input_s3_cleansed_layer,
